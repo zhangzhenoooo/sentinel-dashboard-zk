b57bd1e8ea543e54ac553230fb6154a6889bf3e9
zhangz <1804919062@qq.com>
2023-06-26 18:46:00 +0800

    熔断持久化到zk

---
+++ start.bat
@@ -0,0 +1,4 @@
+d:
+cd D:\zhangz\AAA文档
+start java -Dserver.port=8080 -Dcsp.sentinel.dashboard.server=localhost:8080 -Dproject.name=serviceGovernance -Dcsp.sentinel.config.file=/application.properties -jar sentinel-dashboard-zk.jar
+
